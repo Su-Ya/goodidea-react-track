9dcab55faef8a9bd140122c677a879075e73452c
acid7820@gmail.com <acid7820@gmail.com>
2019-03-29 16:02:02 +0800

    modify index.html/add components folder/add header.js

--- react-exercise/src/index.js
+++ react-exercise/src/index.js
@@ -1,1 +1,6 @@
-console.log('hi');
+console.log('index');
+import header from './components/header.js';
+// var component = require('./src/components/header.js');
+
+
+// document.body.appendChild(component());
